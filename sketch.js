1f45f52e4e97a45e11ab287d4d780eb4ecb2d1f5
gabriel <gabrielrsebastianrios@gmail.com>
2024-11-20 13:37:39 -0500

    se comienza con el codigo todavia no termino

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+var sliders = [];
+
+var angulo = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  //createCanvas(400, 400);
+  for (var i = 0; i < 100; i++) {
+    noCanvas();
+    sliders[i] = createSlider(0, 255, 50);
+  }
+
+  sliders[0].input(adjustSliders);
 }
 
-function draw() {
-  background(220);
+function adjustSliders() {
+  for (var i = 1; i < sliders.length; i++) {
+    sliders[i].value(sliders[0].value());
+  }
 }
