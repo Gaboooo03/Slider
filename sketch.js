865f8e070820196e1723dc2e4638fee46457da98
gabriel <gabrielrsebastianrios@gmail.com>
2024-11-20 22:53:36 -0500

    se termina el codigo de los sliders

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
 var sliders = [];
-
-var angulo = 0;
+var angle = 0;
 
 function setup() {
-  //createCanvas(400, 400);
+  createCanvas(400, 400);
   for (var i = 0; i < 100; i++) {
-    noCanvas();
     sliders[i] = createSlider(0, 255, 50);
   }
-
-  sliders[0].input(adjustSliders);
 }
 
-function adjustSliders() {
-  for (var i = 1; i < sliders.length; i++) {
-    sliders[i].value(sliders[0].value());
+function draw() {
+  var offset = 0;
+  for (var i = 0; i < sliders.length; i++) {
+    var x = map(sin(angle + offset), -1, 1, 0, 255);
+    sliders[i].value(x);
+    offset += 0.02345;
   }
+  angle += 0.01;
 }
